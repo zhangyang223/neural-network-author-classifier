7ea68941b43232a9efca1025bd2ee501e4d5b20a
newmanshee <newman_shee@hotmail.com>
2021-02-07 16:41:45 -0500

    first working version

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 
 let trainedNet;
+let net = new brain.NeuralNetwork();
+let maxSize = 0;
 
-function encode(arg) {
-    return arg.split('').map(x => (x.charCodeAt(0) / 256));
+function encode(arg) 
+{
+    let r =  arg.split('').map(x => (x.charCodeAt(0) / 256));
+    if (r.length < maxSize)
+    {
+        let i = r.length;
+        for (; i< maxSize;i++) r.push(0);
+    }
+    return r;
 }
 
 function processTrainingData(data) {
@@ -14,27 +23,30 @@ function processTrainingData(data) {
     })
 }
 
-function train(data) {
-    let net = new brain.NeuralNetwork();
+function train(data) 
+{
+    maxSize = Math.max.apply(null, data.map(d=>{return d.input.length;}));
     net.train(processTrainingData(data));
-    trainedNet = net.toFunction();
+//    trainedNet = net.toFunction();
 };
 
 function execute(input) {
-    let results = trainedNet(encode(input));
+//    let results = trainedNet(encode(input));
+    console.log(input)
+    let results = net.run(encode(input));
     console.log(results)
     let output;
     let certainty;
-    if (results.trump > results.kardashian) {
-        output = 'Donald Trump'
-        certainty = Math.floor(results.trump * 100)
+    if (results.yes > results.no) {
+        output = 'be funny'
+        certainty = Math.floor(results.yes * 100)
     } else { 
-        output = 'Kim Kardashian'
-        certainty = Math.floor(results.kardashian * 100)
+        output = 'not be funny'
+        certainty = Math.floor(results.no * 100)
     }
 
-    return "I'm " + certainty + "% sure that tweet was written by " + output;
+    return "I'm " + certainty + "% sure that joke will " + output;
 }
 
 train(trainingData);
-console.log(execute("Paste your tweet here"));
+//console.log(execute("Paste your tweet here333333333333333333333333333333333333335555iiixxx  "));
--- js/training-data.js
+++ js/training-data.js
@@ -1,69 +1,73 @@
 
+
 const trainingData = [
     {
-        input: "So true, thank you!",
-        output: { trump: 1 }
-    },{
-        input: "Inside Chi's nursery", 
-        output: { kardashian: 1 }
-    },{
-        input: "Why I dyed my hair pink",
-        output: { kardashian: 1 }
-    },{
-        input: "Feeling Blue (wearing @kkwbeauty powder contour in medium & dark contour kit as eye shadow, & a new lip coming soon)",
-        output: { kardashian: 1 }
-    },{
-        input: "I love my mom so much as a blonde!! @KrisJenner @KUWTK",
-        output: { kardashian: 1 }
-    },{
-        input: "I don’t really do wigs . It’s real.",
-        output: { kardashian: 1 }
-    },{
-        input: "Wait the thought of going back dark makes me sad. @MyleezaKardash may have influenced me",
-        output: { kardashian: 1 }
-    },{
-        input: "I went live on instagram! Thank you my BFF Allison for the fun interview and hank you to Creat & Cultivate for having me!",
-        output: { kardashian: 1 }
-    },{
-        input: "On my way the the @createcultivate event! I’m being interviewed about business by my bff Allison. This should be fun!",
-        output: { kardashian: 1 }
-    },{
-        input: "The sweetest! Best baby! She looks a tiny bit like North and a tiny bit like Saint but definitely her own person!",
-        output: { kardashian: 1 }
-    },{
-        input: "Fashion Week is so much fun in the #KimKardashianGame! I'd love to see what you're wearing! http://smarturl.it/PlayKKH",
-        output: { kardashian: 1 }
-    },{
-        input: "'Congressman Schiff omitted and distorted key facts' @FoxNews  So, what else is new. He is a total phony!",
-        output: { trump: 1 }
-    },{
-        input: "I will be interviewed by @JudgeJeanine on @FoxNews at 9:00 P.M. Enjoy!",
-        output: { trump: 1 }
-    },{
-        input: "Dem Memo: FBI did not disclose who the clients were - the Clinton Campaign and the DNC. Wow!",
-        output: { trump: 1 }
-    },{
-        input: "The Democrat memo response on government surveillance abuses is a total political and legal BUST. Just confirms all of the terrible things that were done. SO ILLEGAL!",
-        output: { trump: 1 }
-    },{
-        input: "Unemployment claims are at the lowest level since 1973. Much of this has to do with the massive cutting of unnecessary and job killing Regulations!",
-        output: { trump: 1 }
-    },{
-        input: "So true Wayne, and Lowest black unemployment in history!",
-        output: { trump: 1 }
-    },{
-        input: "Thank you to the great men and women of the United States @SecretService for a job well done!",
-        output: { trump: 1 }
-    },{
-        input: "Today, @FLOTUS Melania and I were honored to welcome Prime Minister @TurnbullMalcolm and Mrs. Turnbull of Australia to the @WhiteHouse!",
-        output: { trump: 1 }
-    },{
-        input: "After years of rebuilding OTHER nations, we are finally rebuilding OUR nation - and we are restoring our confidence and our pride!",
-        output: { trump: 1 }
-    },{
-        input: "So true, thank you!",
-        output: { trump: 1 }
+        input: "breakthru vaccine has been found, eat garlic, people stay away from you",
+        output: { yes: 1 }
+    },
+    {
+        input: "diamond is a girl's best friend.  Diamond princess is coronavirus' best friend.", 
+        output: { yes: 1 }
+    },
+    {
+        input: "Corona virus is just like pasta The Chinese invented it but the Italians spread it all over the world.", 
+        output: { yes: 1 }
+    },
+    {
+        input: "A child asked his father, How were people born? So his father said, Adam and Eve made babies, then their babies became adults and made babies, and so on. The child then went to his mother, asked her the same question and she told him, We were monkeys then we evolved to become like we are now. The child ran back to his father and said, You lied to me! His father replied, No, your mom was talking about her side of the family.", 
+        output: { yes: 1 }
+    },
+    {
+        input: "Boy: *calls 911* Hello? I need your help!  911: Alright, What is it?  Boy: Two girls are fighting over me!   911: So what's your emergency?  Boy: The ugly one is winning. ", 
+        output: { yes: 1 }
+    },
+    {
+        input: "My wife asked me, What did you buy me for valentines day? Well, I chuckled. You see that pink Mercedes over there?  Yes, she said happily.  Well I bought you a toothbrush the same colour.", 
+        output: { yes: 1 }
+    },
+    {
+        input: "What's the difference between love and marriage? Love is blind. Marriage is an eye-opener.", 
+        output: { no: 1 }
+    },
+    {
+        input: "I said, I love you so much. I could never live without you. My girlfriend giggled and asked, Is that you talking or the beer? I said, It's me talking to the beer...", 
+        output: { yes: 1 }
+    },
+    {
+        input: "A woman noticed her husband standing on the bathroom scale, sucking in his stomach. Ha­­! That’s not going to help, she said. Sure, it does, he said. It’s the only way I can see the numbers.", 
+        output: { yes: 1 }
+    },
+    {
+        input: "If you had a choice between two superpowers, being invisible or flying, which would you choose?", 
+        output: { yes: 1 }
+    },
+    {
+        input: "A husband asks his wife: If I died, would you marry again? Oh darling, of course I wouldn’t. I’d go and live with my sister. And if I died, would you remarry? No, I think I’d go and live with your sister too.", 
+        output: { no: 1 }
+    },
+    {
+        input: "Honey, I’m so sorry that I let out my anger at you so often. How do you manage to stay so calm with my foul moods? I always go and clean the toilet when that happens.And that helps?Yes, because I’m using your toothbrush.", 
+        output: { no: 1 }
+    },
+    {
+        input: "We had a terrible row with my wife last night. But she crawled to me on her knees in the end. – Wow – that’s really impressive! What did she say?! – Come out from under that sofa, you filthy coward!", 
+        output: { no: 1 }
+    },
+    {
+        input: "The stock market is weird. Every time one guy sells, another one buys, and they both think they're smart.", 
+        output: { yes: 1 }
+    },
+    {
+        input: "My wife’s credit card got stolen the other day. I haven’t bothered reporting it, though, because the thief spends much less than my wife.", 
+        output: { no: 1 }
+    },
+    {
+        input: "As internet movies become widely availabe, the usage of tissue paper goes up as well.  Women use more to wipe their tears and men use it to", 
+        output: { no: 1 }
     }
+    
+
+
 ]
 
 
